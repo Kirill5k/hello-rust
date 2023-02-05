3e9d9c8fb1c29d7cb49b9b0dea78941ccb3cf760
Kirill5k <immotional@aol.com>
2023-02-05 15:23:58 +0000

    Add more examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+
 fn main() {
-    println!("Hello, World!");
+    let var = "World";
+    let unicode_char = '\u{261D}';
+    let numbers_array = [1, 2, 3, 4, 5];
+    let tuple = (10, 3.14, "string");
+    println!("Hello, {var}!{unicode_char}");
+    println!("array {numbers_array:?} of length {}", numbers_array.len());
+    println!("first item from tuple is {}", tuple.0)
 }
