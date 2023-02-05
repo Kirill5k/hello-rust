a1d1e01e6a9f8132d7e47357c25188c256391fd1
kirill5k <immotional@aol.com>
2023-02-05 15:41:10 +0000

    Add function examples

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    let var = "World";
-    let unicode_char = '\u{261D}';
     let numbers_array = [1, 2, 3, 4, 5];
     let tuple = (10, 3.14, "string");
-    println!("Hello, {var}!{unicode_char}");
+    say_hello("World");
     println!("array {numbers_array:?} of length {}", numbers_array.len());
     println!("first item from tuple is {}", tuple.0)
+}
+
+fn say_hello(name: &str) {
+    let unicode_char = '\u{261D}';
+    println!("Hello, {name}!{unicode_char}")
+}
+
+// lack of the semicolon defines the return
+fn sum(x: i32, y: i32) -> i32 {
+    x + y
 }
