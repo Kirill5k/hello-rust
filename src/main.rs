49846155a211f8e4402b5353f5c78eeb4433e7e2
kirill5k <immotional@aol.com>
2023-02-06 12:15:39 +0000

    Loop example

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,15 @@ fn main() {
     let tuple = (10, 3.14, "string");
     say_hello("World");
     println!("array {numbers_array:?} of length {}", numbers_array.len());
-    println!("first item from tuple is {}", tuple.0)
+    println!("first item from tuple is {}", tuple.0);
+
+    let mut count = 0;
+    let result = loop {
+        if count == 10 { break count * 10 }
+        count += 1;
+        println!("count is {count}")
+    };
+    println!("loop result is {}", sum(result, 0))
 }
 
 fn say_hello(name: &str) {
