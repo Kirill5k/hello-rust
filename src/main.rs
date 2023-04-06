1094cd51d37916e4c2298e19821a982f56c83fea
kirill5k <immotional@aol.com>
2023-04-06 10:26:08 +0100

    Return basic response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -21,5 +22,11 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {:#?}", http_request);
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let content_length = format!("Content-Length: {}", contents.len());
+
+    let response = format!("{status_line}\r\n{content_length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
