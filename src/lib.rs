30c5f029926befe5048f00fefde275d106f5ae11
kirill5k <immotional@aol.com>
2023-04-06 11:15:10 +0100

    Worker interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,39 @@
-pub struct ThreadPool;
+use std::thread;
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
+    }
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        assert!(size > 0);
+
+        let workers = (0..size)
+            .into_iter()
+            .map(Worker::new)
+            .collect();
+
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
