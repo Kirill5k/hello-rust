9ac1acd5c018a9520c978f75a713585755712edc
kirill5k <immotional@aol.com>
2023-04-11 14:34:30 +0100

    Move macro into examples folder

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-#[macro_export]
-macro_rules! vec {
-    ( $( $x:expr ),* ) => {
-        {
-            let mut temp_vec = Vec::new();
-            $(
-                temp_vec.push($x);
-            )*
-            temp_vec
-        }
-    };
-}
 
 fn main() {
     println!("Hello, World!");
