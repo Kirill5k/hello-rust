4f515e197ed01a361416a42eced8567fbaf2365a
kirill5k <immotional@aol.com>
2023-04-11 14:53:05 +0100

    Move closures into examples folder

---
+++ examples/closures.rs
@@ -0,0 +1,17 @@
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
+fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
+fn main() {
+    let add_one = |x: i32| {
+        x + 1
+    };
+
+    let answer = do_twice(add_one, 5);
+
+    println!("The answer is: {}", answer);
+}
