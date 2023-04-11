5da1e1adb6cb47bc13654318207dfb685c490eb5
kirill5k <immotional@aol.com>
2023-04-11 14:52:25 +0100

    Advanced closure examples

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
     f(arg) + f(arg)
 }
 
+fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
 fn main() {
     let add_one = |x: i32| {
         x + 1
